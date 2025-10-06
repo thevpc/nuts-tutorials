8700b639ee613b33c2b35e1aaac625b459c37af4
thevpc <taha.bensalah@gmail.com>
2025-10-06 04:26:50 +0100

    Update ExamplesOfZip.java : bump nuts 0.8.7

--- lib/lib-integration-example/src/main/java/net/thevpc/nuts/tutorial/lib/ExamplesOfZip.java
+++ lib/lib-integration-example/src/main/java/net/thevpc/nuts/tutorial/lib/ExamplesOfZip.java
@@ -1,7 +1,7 @@
 package net.thevpc.nuts.tutorial.lib;
 
-import net.thevpc.nuts.NOut;
-import net.thevpc.nuts.NSession;
+import net.thevpc.nuts.io.NOut;
+import net.thevpc.nuts.core.NSession;
 import net.thevpc.nuts.io.*;
 import net.thevpc.nuts.util.NMsg;
 
