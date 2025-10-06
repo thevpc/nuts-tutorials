58b99ef602dd8e330257b52805ab89df5fed97c9
thevpc <taha.bensalah@gmail.com>
2025-10-06 04:26:50 +0100

    Update CustomCliA.java : bump nuts 0.8.7

--- naf/custom-cli-project-a/src/main/java/net/thevpc/nuts/tutorial/cli/CustomCliA.java
+++ naf/custom-cli-project-a/src/main/java/net/thevpc/nuts/tutorial/cli/CustomCliA.java
@@ -3,7 +3,10 @@ package net.thevpc.nuts.tutorial.cli;
 import java.util.ArrayList;
 import java.util.List;
 
-import net.thevpc.nuts.*;
+import net.thevpc.nuts.app.NApp;
+import net.thevpc.nuts.app.NAppDefinition;
+import net.thevpc.nuts.app.NAppRunner;
+import net.thevpc.nuts.io.NOut;
 import net.thevpc.nuts.util.NMsg;
 import net.thevpc.nuts.cmdline.NCmdLine;
 import net.thevpc.nuts.util.NRef;
