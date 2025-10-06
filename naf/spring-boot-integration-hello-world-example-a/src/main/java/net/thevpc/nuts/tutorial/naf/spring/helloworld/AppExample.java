50d5e59647d26fdbcb5ef234f6305b5a2a649a59
thevpc <taha.bensalah@gmail.com>
2025-10-06 04:26:51 +0100

    Update AppExample.java : bump nuts 0.8.7

--- naf/spring-boot-integration-hello-world-example-a/src/main/java/net/thevpc/nuts/tutorial/naf/spring/helloworld/AppExample.java
+++ naf/spring-boot-integration-hello-world-example-a/src/main/java/net/thevpc/nuts/tutorial/naf/spring/helloworld/AppExample.java
@@ -1,6 +1,9 @@
 package net.thevpc.nuts.tutorial.naf.spring.helloworld;
 
-import net.thevpc.nuts.*;
+import net.thevpc.nuts.app.NApp;
+import net.thevpc.nuts.app.NAppDefinition;
+import net.thevpc.nuts.app.NAppRunner;
+import net.thevpc.nuts.io.NOut;
 import net.thevpc.nuts.util.NMsg;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,6 +17,6 @@ public class AppExample {
 
     @NAppRunner
     public void run() {
-        NOut.println(NMsg.ofC("Hello ##World## from %s",NApp.of().getId().get()));
+        NOut.println(NMsg.ofC("Hello ##World## from %s", NApp.of().getId().get()));
     }
 }
