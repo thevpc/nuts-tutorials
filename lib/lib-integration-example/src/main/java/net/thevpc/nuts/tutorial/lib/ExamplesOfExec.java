a461453af1802c66641f3c6b3a70ada93683705a
thevpc <taha.bensalah@gmail.com>
2025-10-08 23:49:40 +0100

    Update ExamplesOfExec.java : refactor NMsg

--- lib/lib-integration-example/src/main/java/net/thevpc/nuts/tutorial/lib/ExamplesOfExec.java
+++ lib/lib-integration-example/src/main/java/net/thevpc/nuts/tutorial/lib/ExamplesOfExec.java
@@ -3,7 +3,7 @@ package net.thevpc.nuts.tutorial.lib;
 import net.thevpc.nuts.command.NExecCmd;
 import net.thevpc.nuts.io.NExecInput;
 import net.thevpc.nuts.io.NOut;
-import net.thevpc.nuts.util.NMsg;
+import net.thevpc.nuts.text.NMsg;
 
 public class ExamplesOfExec {
     public void executeAll() {
